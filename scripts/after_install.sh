cd035263377981177263d3dfb9721e2851fae665
Anup Sinha <anupsinha@Anups-MacBook-Air.local>
2023-08-25 15:54:25 +0530

    latest

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-#_Change_Working_Directory
-cd /home/ubuntu/server
-
-#_Remove_Unused_Code
-rm -rf node_modules
-rm -rf build
-
-#Install_node_modules_&_Make_React_Build
-npm  install --save-dev @babel/plugin-proposal-private-property-in-object
-npm run build
+isExistApp = `pgrep httpd`
+if [[ -n  $isExistApp ]]; then
+    service httpd stop        
+fi
